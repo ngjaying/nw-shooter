b34723c2982718ef492f1a5ba945acc0542e98bc
ngjaying <ngjaying@gmail.com>
2014-04-19 15:26:13 +0800

    The first available copy

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
-(function(){
-	var ns = require('node-shooter'),
-	fileControl = document.getElementById('inputFile'),
-	messageDiv = document.getElementById('divMessage');
-	fileControl.addEventListener('change', function(e){
-		var file = fileControl.value;
-		ns.getSub(file, null, function(err){
-			//Stop progress here
-			if(err){
-				messageDiv.innerHTML = 'Subtitle download fail for ' + file + '<br/>Error: ' + err;
-			}else{
-				messageDiv.innerHTML = 'Subtitle download successful for ' + file;
+var ns = require('node-shooter');
+window.onload = function(){
+	(function() {
+		var fileControl = document.getElementById('inputFile'),
+		fileUploader = document.getElementById('divFile'),
+		messageDiv = document.getElementById('messageDiv'),
+		VIDEOEXTS = ['.avi','.mp4','.mkv','.rmvb','.rm','.asf','.divx','.mpg','.mpeg','.mpe','.wmv','.vob'];
+
+		fileControl.addEventListener('change', function(e) {
+			var files = fileControl.files, message = [];
+			for (var i = 0; i < files.length; i++) {
+				filterVideo(files[i]) && createDownloader(files[i]);
 			}
-			
-		});
-	});
-})();
+		}, false);
+
+		fileUploader.addEventListener('click', function(e) {
+			fileControl.click();
+		}, false);
+
+		function createDownloader(file) {
+			var d = document.createElement('div');
+			d.innerText = file.name;
+			d.className = 'loading';				
+
+			ns.getSub(file.path, null, function(err){
+				if(err){
+					d.className = 'error';
+					d.innerText = file.name + '字幕不存在';
+				}else{
+					d.className = 'success';
+				}
+
+			});
+			messageDiv.appendChild(d);
+		}
+
+		function filterVideo(file){
+			return file.size > 4194304 && checkExtension(file.name);
+		}
+
+		function checkExtension(fileName){
+			var ext = fileName.split('.');
+			ext = '.' + ext[ext.length - 1];
+			return VIDEOEXTS.some(function(ele){
+				return (ele == ext);
+			});
+		}
+	})();
+};
